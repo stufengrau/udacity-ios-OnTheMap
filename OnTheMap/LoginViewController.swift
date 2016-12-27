f32a3affb02b1ac95ac350692f0f8af53261e6e7
stufengrau <stufengrau@gmail.com>
2016-12-27 21:55:34 +0100

    Add UI for Login

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnTheMap
 //
 //  Created by heike on 27/12/2016.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
