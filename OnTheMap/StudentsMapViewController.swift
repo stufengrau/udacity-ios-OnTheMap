b3ca9f433b90acda67609b8c4d1cd8142739f9d8
stufengrau <stufengrau@gmail.com>
2016-12-29 21:47:18 +0100

    Show StudentLocations and Pin Annotations on Map
    - tap a pin -> annotation popup is displayed
    - tap inside annotation popup -> open URL in Safari

--- OnTheMap/StudentsMapViewController.swift
+++ OnTheMap/StudentsMapViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import MapKit
 
 class StudentsMapViewController: UIViewController, MKMapViewDelegate {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
 
     var students: [StudentInformation] {
         return (UIApplication.shared.delegate as! AppDelegate).studentInformations
@@ -17,13 +20,109 @@ class StudentsMapViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        getStudentInformations()
     }
+    
+    private func getStudentInformations() {
+        debugPrint("getStudentInformations called.")
+        ParseAPI.sharedInstance().getStudentLocations { (networkRequestResult, studentInformations) in
+            switch(networkRequestResult) {
+            case .networkFailure:
+                self.showAlert("Download of Student Informations failed. Try again later.")
+            case .success:
+                (UIApplication.shared.delegate as! AppDelegate).studentInformations = studentInformations
+                debugPrint(self.students.count)
+                self.refreshData()
+            }
+        }
+    }
+    
+    private func createAnnotations() {
+        
+        debugPrint("createAnnotations called.")
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        var annotations = [MKPointAnnotation]()
+        
+        for student in students {
+            
+            // Notice that the float values are being used to create CLLocationDegree values.
+            // This is a version of the Double type.
+            let lat = CLLocationDegrees(student.latitude)
+            let long = CLLocationDegrees(student.longitude)
+            
+            // The lat and long are used to create a CLLocationCoordinates2D instance.
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            let first = student.firstName
+            let last = student.lastName
+            let mediaURL = student.mediaURL
+            
+            // Here we create the annotation and set its coordiate, title, and subtitle properties
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(first) \(last)"
+            annotation.subtitle = mediaURL
+            
+            // Finally we place the annotation in an array of annotations.
+            annotations.append(annotation)
+        }
+        
+        // When the array is complete, we add the annotations to the map.
+        debugPrint(annotations.count)
+        debugPrint(annotations[0])
+        self.mapView.addAnnotations(annotations)
+
+    }
+    
+    private func refreshData() {
+        DispatchQueue.main.async {
+            self.createAnnotations()
+      }
+    }
+    
+    private func showAlert(_ errormessage: String) {
+        DispatchQueue.main.async {
+            //self.enableUI(true)
+            let alertController = UIAlertController(title: "", message: errormessage, preferredStyle: .alert)
+            let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+            alertController.addAction(dismissAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - MKMapViewDelegate
+    
+    // Here we create a view with a "right callout accessory view". You might choose to look into other
+    // decoration alternatives. Notice the similarity between this method and the cellForRowAtIndexPath
+    // method in TableViewDataSource.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    
+    // This delegate method is implemented to respond to taps. It opens the system browser
+    // to the URL specified in the annotationViews subtitle property.
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            if let toOpen = view.annotation?.subtitle! {
+                UIApplication.shared.open(URL(string: toOpen)!, options: [:], completionHandler: nil)
+            }
+        }
     }
     
 
