0604fd1352ad7fe0456b037a1a4df12d08dafdf7
stufengrau <stufengrau@gmail.com>
2016-12-29 15:59:02 +0100

    Display StudentLocations in the Table View

--- OnTheMap/StudentsTableViewController.swift
+++ OnTheMap/StudentsTableViewController.swift
@@ -18,6 +18,7 @@ class StudentsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getStudentInformations()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -35,23 +36,55 @@ class StudentsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        debugPrint(students.count)
+        return students.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "studentName", for: indexPath) as! StudentsTableViewCell
+        
+        let student = students[indexPath.row]
+        
+        cell.studentsName.text = "\(student.firstName) \(student.lastName)"
+        
         return cell
     }
-    */
+    
+    private func getStudentInformations() {
+        debugPrint("getStudentInformations called.")
+        ParseAPI.sharedInstance().getStudentLocations { (networkRequestResult, studentInformations) in
+            switch(networkRequestResult) {
+            case .networkFailure:
+                self.showAlert("Download of Student Informations failed. Try again later.")
+            case .success:
+                (UIApplication.shared.delegate as! AppDelegate).studentInformations = studentInformations
+                self.refreshData()
+            }
+        }
+    }
+    
+    private func refreshData() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    private func showAlert(_ errormessage: String) {
+        DispatchQueue.main.async {
+            //self.enableUI(true)
+            let alertController = UIAlertController(title: "", message: errormessage, preferredStyle: .alert)
+            let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+            alertController.addAction(dismissAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+
 
     /*
     // Override to support conditional editing of the table view.
