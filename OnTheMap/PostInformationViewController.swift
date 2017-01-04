ce64f8d2b8d1eeb64d85e0156906bc447c7b0a4c
stufengrau <stufengrau@gmail.com>
2017-01-04 21:10:16 +0100

    Submit Location and Link as a new Student Location
    - enable buttons after text was entered
    - check if url begins with a protocol and prefix it, if necessary
    - when keyboard shows, move view up, to make location text field visible
    - after successfully submitting the new location, trigger new request of the data from server

--- OnTheMap/PostInformationViewController.swift
+++ OnTheMap/PostInformationViewController.swift
@@ -11,6 +11,7 @@ import MapKit
 
 class PostInformationViewController: UIViewController, MKMapViewDelegate, UITextFieldDelegate {
     
+    // MARK: Properties
     @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
     @IBOutlet weak var activityIndicatorOuterView: UIView!
     
@@ -22,17 +23,23 @@ class PostInformationViewController: UIViewController, MKMapViewDelegate, UIText
     @IBOutlet weak var submitInformationButton: UIButton!
     @IBOutlet weak var mapView: MKMapView!
     
-    var long: Double?
-    var lat: Double?
+    var longitude: Double?
+    var latitude: Double?
+    
+    var instanceOfStudentsLocationsVC: StudentsLocationsViewController?
 
+    // MARK: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        // disable Button until a location is entered
         findLocationButton.isEnabled = false
         
         location.delegate = self
         mediaURL.delegate = self
         
+        // hide the second stackView until a location string
+        // was successfully geocoded
         enterLinkStackView.isHidden = true
         
         activityIndicatorView.hidesWhenStopped = true
@@ -40,23 +47,42 @@ class PostInformationViewController: UIViewController, MKMapViewDelegate, UIText
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+
+
+    // MARK: IBActions
+    
     @IBAction func cancelPosting(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    
+    // location text field is placed inside another view
+    // when user taps inside the view, activate text field
     @IBAction func acitvateLocationTextField(_ sender: UITapGestureRecognizer) {
         location.becomeFirstResponder()
     }
     
+    // geocodes a location string
     @IBAction func findLocation(_ sender: UIButton) {
         
+        // location should contain a value by now
         guard let locationString = location.text else {
             assertionFailure("Impossible: location can not be empty after entering location")
             return
         }
         
+        // display an activity indicator
         enableUI(false)
+        
+        // geocode location string
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(locationString) { (placemarks, error) in
             
@@ -68,59 +94,117 @@ class PostInformationViewController: UIViewController, MKMapViewDelegate, UIText
                     return
             }
             
-            self.long = coordinates.longitude
-            self.lat = coordinates.latitude
+            // extract longitude and latitude
+            self.longitude = coordinates.longitude
+            self.latitude = coordinates.latitude
             
+            // add annotation to the map, center the map view around this coordinates
+            // and set the span to zoom in
             self.mapView.addAnnotation(MKPlacemark(placemark: placemark))
             self.mapView.region = MKCoordinateRegion(center: coordinates, span: MKCoordinateSpanMake(0.1, 0.1))
             
+            // hide this stack view and enable the stack view 
+            // to provide a link and to submit the new location
             self.locationStackView.isHidden = true
             self.enterLinkStackView.isHidden = false
+            // button is disabled, until a url was entered
             self.submitInformationButton.isEnabled = false
-
         }
     }
-    
-    @IBAction func submitInformation(_ sender: UIButton) {
-    
-    }
 
-    private func enableUI(_ enabled: Bool) {
-        DispatchQueue.main.async {
-            
-            if enabled {
-                self.activityIndicatorView.stopAnimating()
-                self.activityIndicatorOuterView.isHidden = true
-            } else {
-                self.activityIndicatorView.startAnimating()
-                self.activityIndicatorOuterView.isHidden = false
+    // Submits the new location
+    @IBAction func submitInformation(_ sender: UIButton) {
+        
+        // all properties should be set by now
+        guard let mediaURLString = mediaURL.text, let location = location.text, let latitude = latitude, let longitude = longitude else {
+            assertionFailure("Impossible: mediaURL, location, longitude and latitude can not be empty after entering location and mediaURL")
+            return
+        }
+        
+        var checkedURL: String
+        
+        // make sure, that the url begins with a protocol
+        if(doesURLBeginWithProtocol(url: mediaURLString)) {
+            checkedURL = mediaURLString
+        } else {
+            checkedURL = prefixURLWithProtocol(url: mediaURLString)
+        }
+        
+        // display an activity indicator
+        enableUI(false)
+        
+        // try to post the new location (updateStudentLocation checks, if a location already exists and updates
+        // the location, if needed)
+        ParseAPI.sharedInstance().updateStudentLocation(location, checkedURL, latitude, longitude) { (networkRequestResult) in
+            self.enableUI(true)
+            switch(networkRequestResult) {
+            case .networkFailure:
+                self.showAlert("Posting your location failed. Please try again later.")
+            case .success:
+                // after dismissing the view, request new data from server
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true, completion: {
+                        self.instanceOfStudentsLocationsVC?.getStudentsInformation()
+                    })
+                }
             }
         }
     }
     
+    // MARK: Text Field Delegate
+    // dismiss keyboard
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
     
+    // enable buttons when text was entered in text fields
     func textFieldDidEndEditing(_ textField: UITextField) {
-        if let location = location.text, !location.isEmpty {
-            findLocationButton.isEnabled = true
+        if let location = location.text {
+            findLocationButton.isEnabled = !location.isEmpty
         }
         
-        if let mediaURL = mediaURL.text, !mediaURL.isEmpty {
-            submitInformationButton.isEnabled = true
+        if let mediaURL = mediaURL.text {
+            submitInformationButton.isEnabled = !mediaURL.isEmpty
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: NSNotification - Keyboard Notifications
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(PostInformationViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(PostInformationViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
-    */
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        // location text field should be visible when editing -> shift view up
+        if location.isEditing {
+            locationStackView.frame.origin.y -= 100
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        locationStackView.frame.origin.y = 0
+    }
+
+    // MARK: Helper Methods
+    // if UI should be disabled:
+    // show a semitransparent view with an activity indicator
+    // which overlays the main view
+    func enableUI(_ enabled: Bool) {
+        DispatchQueue.main.async {
+            if enabled {
+                self.activityIndicatorView.stopAnimating()
+                self.activityIndicatorOuterView.isHidden = true
+            } else {
+                self.activityIndicatorOuterView.isHidden = false
+                self.activityIndicatorView.startAnimating()
+            }
+        }
+    }
 
 }
--- OnTheMap/StudentsLocationsViewController.swift
+++ OnTheMap/StudentsLocationsViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
+// Custom View Controller Class to provide some shared functions
+// for the StudentsMap and StudentsTable View Controller
+// to avoid code duplication
 class StudentsLocationsViewController: UIViewController {
     
+    // get students information from server
     func getStudentsInformation() {
-        debugPrint("getStudentInformations called.")
         enableUI(false)
         ParseAPI.sharedInstance().getStudentsLocations { (networkRequestResult) in
             self.enableUI(true)
@@ -19,12 +22,14 @@ class StudentsLocationsViewController: UIViewController {
             case .networkFailure:
                 self.showAlert("Download of Student Informations failed. Please try again later.")
             case .success:
-                debugPrint(ParseAPI.sharedInstance().studentInformations.count)
                 self.refreshData()
             }
         }
     }
     
+    // check if the user has already posted a location
+    // if true, ask if the existing location should be overwriten
+    // else modally present the view to enter a new location and url
     func checkStudentLocation() {
         enableUI(false)
         ParseAPI.sharedInstance().getStudentLocation { (networkRequestResult) in
@@ -32,38 +37,45 @@ class StudentsLocationsViewController: UIViewController {
             switch(networkRequestResult) {
             case .networkFailure:
                 self.showAlert("Could not check location status. Please try again later.")
-            case .locationExists:
-                debugPrint("User already posted a location")
-                self.overrideLocationAlert("You already have posted a location. Do you want to override it?")
-            case .noLocationExists:
+            case .locationDoesExist:
+                self.overrideLocationAlert("You have already posted a location. Do you want to overwrite your current location?")
+            case .locationDoesNotExist:
                 self.presentPostingInformationView()
             }
         }
     }
     
+    // show the Overwrite Alert Message
+    // if Overwrite is selected, modally present the view to enter a new location and url
     func overrideLocationAlert(_ message: String) {
         DispatchQueue.main.async {
             let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default, handler: {_ in
+            let overwriteAction = UIAlertAction(title: "Overwrite", style: .default, handler: {_ in
                 self.presentPostingInformationView()
             })
             let dismissAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-            
-            alertController.addAction(okAction)
+    
+            alertController.addAction(overwriteAction)
             alertController.addAction(dismissAction)
             
             self.present(alertController, animated: true, completion: nil)
         }
     }
     
+    // modally present the view to enter a new location and url
     func presentPostingInformationView() {
         DispatchQueue.main.async {
             let controller = self.storyboard!.instantiateViewController(withIdentifier: "postInformationView")
+            let postController = controller.childViewControllers[0] as! PostInformationViewController
+            postController.instanceOfStudentsLocationsVC = self
             self.present(controller, animated: true, completion: nil)
         }
     }
     
-
+    // just trigger the logout request and dismiss view immediately
+    // if the logout network request fails, nothing bad happens
+    // Alternative: if the logout network request fails, show an alert
+    // and do not dismiss the view ...
     func logoutAndDismiss() {
         UdacityAPI.sharedInstance().logout()
         self.dismiss(animated: true, completion: nil)
